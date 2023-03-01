beff9f5d62a4d2bb83051079f0eea8ff2cfd7980
Sufiyan <sufiyankarippol10@gmail.com>
2023-03-01 15:43:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+import 'presntation/main_page/widgets/screen_main.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home:const ScreenMain() , 
+    );
+  }
+}
---
+++ lib/presntation/main_page/widgets/screen_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenMain extends StatelessWidget {
+  const ScreenMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text(
+          'main Page',
+        ),
+      )),
+    );
+  }
+}
